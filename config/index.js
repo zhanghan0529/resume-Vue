0019d9c1c515477c7ca875de5c627628bcbb0356
zhanghan0529 <zhanghan2418@163.com>
2017-11-24 22:23:32 +0800

    使用Vuex完成数据的单向绑定，以及输入区域的布局

--- config/index.js
+++ config/index.js
@@ -54,7 +54,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, "../dist"),
     assetsSubDirectory: "static",
-    assetsPublicPath: "/resumer/dist",
+    assetsPublicPath: "/resume/dist",
 
     /**
      * Source Maps
